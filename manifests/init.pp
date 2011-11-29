1d7516447c0c5485df22682cb18ff41f0daa5f5b
Nick Lewis <nick@puppetlabs.com>
2011-11-29 14:44:33 -0800

    bundle install is working

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,11 +11,32 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class chili-project {
-  vcsrepo { '/root/chili-project':
+class chili-project($bundle_executable='/usr/bin/bundle', $version) {
+  Package[bundler] -> Exec[bundle_install]
+  Vcsrepo[chili_source] -> Exec[bundle_install]
+
+# Step 1 clone source
+  vcsrepo { chili_source:
     ensure   => present,
-    revision => stable,
+    path     => '/root/chili-project',
+    revision => $version,
     source   => 'git://github.com/chiliproject/chiliproject.git',
     provider => git,
+  }
+
+# Step 2 install gems with bundler
+  package { bundler:
+    ensure => present,
+    provider => gem,
+  }
+
+  package { 'librmagick-ruby':
+    ensure => present,
+  }
+
+  exec { bundle_install:
+    logoutput => true,
+    unless => "$bundle_executable check",
+    command => "$bundle_executable install --without=mysql mysql2 sqlite postgres rmagick",
   }
 }
