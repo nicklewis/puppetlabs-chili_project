44b97aa0da49b397afb94502d3a8bd760d2e4828
Deepak Giridharagopal <deepak@puppetlabs.com>
2011-12-09 15:30:37 -0700

    Add explicit parameter for db_adapter
    Part of externalizing database configuration requires that the user specify the
    full set of details about their database environment. Previously, we'd
    hard-coded the use of 'mysql'. Now the user has to specify the particular
    database adapter they'd like to use.

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,6 +19,7 @@ class chili_project(
   $source_repo='git://github.com/chiliproject/chiliproject.git',
   $user='chili',
   $group='chili',
+  $db_adapter,
   $db_user='chili',
   $db_pass,
   $db_name='chili_project',
@@ -26,15 +27,15 @@ class chili_project(
   $db_port='3306',
   $load_default_data=true
 ) {
-  Class[chili_project::media] ->
-  Class[chili_project::build] ->
-  Class[chili_project::configuration] ->
-  Class[chili_project::database] ->
-  Class[chili_project::accounts]
-
   include chili_project::media
   include chili_project::build
   include chili_project::configuration
   include chili_project::database
   include chili_project::accounts
+
+  Class[chili_project::media] ->
+  Class[chili_project::build] ->
+  Class[chili_project::configuration] ->
+  Class[chili_project::database] ->
+  Class[chili_project::accounts]
 }
